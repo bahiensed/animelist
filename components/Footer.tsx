6e850bd0076cadc0cddeb1ab2c0a93fa4ac65229
dbahiense <douglas@rohling.com.br>
2024-07-19 14:41:06 -0300

    change hero and footer images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
-      <Image
-        src="./logo.svg"
-        alt="logo"
-        width={47}
-        height={44}
-        className="object-contain"
-      />
+      <p className="text-base font-bold text-white">
+        <Link href={"https://bahien.se"}>&copy; dbahiense, 2024</Link>
+      </p>
+      <Link href={"https://www.rohling.com.br"}>
+        <Image
+          src="./logo.svg"
+          alt="logo"
+          width={47}
+          height={44}
+          className="object-contain"
+        />
+      </Link>
+
       <div className="flex items-center gap-6">
         <Image
           src="./tiktok.svg"
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Hero() {
   return (
     <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
       <div className="flex-1 flex flex-col gap-8">
-        <Image
-          src="/logo.png"
-          alt="logo"
-          width={101}
-          height={96}
-          className="object-contain"
-        />
+        <Link href={"https://www.rohling.com.br"}>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={128}
+            height={122}
+            className="object-contain"
+          />        
+        </Link>
+
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
           We both really <span className="red-gradient">love to Watch</span> Anime shows
         </h1>
       </div>
 
-      <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
+      <div className="lg:flex-1 relative w-full h-[50vh] justify-bottom">
         <Image src="/anime.png" alt="anime" fill className="object-contain" />
       </div>
     </header>
